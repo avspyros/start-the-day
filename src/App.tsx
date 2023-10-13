d0df5a23965299967db1cd377df5ffc101d3bea0
Spyros <avspyros@gmail.com>
2023-10-13 20:55:18 +0300

    added spinner to Quotes Widget and local storage to Settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Wrapper from './components/Wrapper';
 import Spacer from './components/Spacer';
 import Settings from './components/Settings';
@@ -12,12 +12,11 @@ interface WidgetVisibility {
   hideTaskWidget: boolean;
 }
 
+// Load the initial widget visibility state from local storage, or set defaults
+const initialWidgetVisibility: WidgetVisibility = JSON.parse(localStorage.getItem('widgetVisibility') || '{"hideWeatherWidget": false, "hideQuoteWidget": false, "hideTaskWidget": false}');
+
 export default function App() {
-  const [widgetVisibility, setWidgetVisibility] = useState<WidgetVisibility>({
-    hideWeatherWidget: false,
-    hideQuoteWidget: false,
-    hideTaskWidget: false
-  });
+  const [widgetVisibility, setWidgetVisibility] = useState<WidgetVisibility>(initialWidgetVisibility);
 
   const toggleWidgetVisibility = (widgetName: keyof WidgetVisibility) => {
     setWidgetVisibility(prevState => ({
@@ -25,6 +24,11 @@ export default function App() {
       [widgetName]: !prevState[widgetName]
     }));
   };
+
+  // Use useEffect to save the widget visibility state to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem('widgetVisibility', JSON.stringify(widgetVisibility));
+  }, [widgetVisibility]);
 
   return (
     <Wrapper>
--- src/quotes-widget/QuotesWidget.tsx
+++ src/quotes-widget/QuotesWidget.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Box, Flex, Heading, Text, IconButton } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, IconButton, Spinner } from '@chakra-ui/react';
 import { RepeatIcon } from '@chakra-ui/icons';
 import { boxStyles } from '../globalStyles';
 
@@ -10,21 +10,30 @@ function QuotesWidget() {
     source: ''
   });
   const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState(false);
+
+  let quotesArr = [];
 
   const generateQuote = () => {
     const controller = new AbortController();
+    setLoading(true);
     axios
       .get('https://philosophy-quotes-api.glitch.me/quotes', { signal: controller.signal })
       .then(res => {
-        let randomQuoteIndex = Math.floor(Math.random() * res.data.length);
-        let randomQuote = res.data[randomQuoteIndex];
+        quotesArr = res.data;
+        // console.log(quotesArr);
+        let randomQuoteIndex = Math.floor(Math.random() * quotesArr.length);
+        let randomQuote = quotesArr[randomQuoteIndex];
         // console.log(randomQuote);
         setError('');
         setQuoteResult(randomQuote);
+        setLoading(false);
       })
       .catch(err => {
+        console.log(err);
         setQuoteResult({ quote: '', source: '' });
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
@@ -48,13 +57,17 @@ function QuotesWidget() {
           {error}
         </Text>
       )}
-
-      <Text as="cite" color="orange.400">
-        {quoteResult && `"${quoteResult.quote}"`}
-      </Text>
-      <Flex mt="1rem;" justify="end">
-        <Text color="gray.400">{quoteResult.source}</Text>
-      </Flex>
+      {loading && <Spinner color="orange.400" />}
+      {quoteResult && (
+        <>
+          <Text as="cite" color="orange.400">
+            {quoteResult.quote}
+          </Text>
+          <Flex mt="1rem;" justify="end">
+            <Text color="gray.400">{quoteResult.source}</Text>
+          </Flex>
+        </>
+      )}
     </Box>
   );
 }
