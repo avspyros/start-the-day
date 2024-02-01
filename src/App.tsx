6c311ec9084a253ef9ac3b0205c554c3706106cd
Spyros <avspyros@gmail.com>
2024-02-01 16:54:44 +0200

    Before delete branches

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const initialWidgetVisibility: WidgetVisibility = JSON.parse(
 );
 
 // Fetch BG from local storage
+
 const initialBG =
   localStorage.getItem('BG') ||
   'https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80';
